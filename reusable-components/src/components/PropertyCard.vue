e87bb85daa263836b81815b6a30c1a4a7947cd18
Joy Poddar <joypoddar.sd@gmail.com>
2021-01-04 09:55:33 +0530

    Making Text Content Feel Designed

--- reusable-components/src/components/PropertyCard.vue
+++ reusable-components/src/components/PropertyCard.vue
@@ -2,16 +2,23 @@
   <div class="bg-white border rounded-lg overflow-hidden">
     <img :src="property.imageUrl" :alt="property.imageAlt" />
     <div class="p-6">
-      <div>{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
-      <h4>
+      <div class="text-gray-600 text-xs uppercase font-semibold tracking-wide">
+        {{ property.beds }} beds &bull; {{ property.baths }} baths
+      </div>
+      <h4 class="font-semibold text-lg leading-tight truncate">
         {{ property.title }}
       </h4>
-      <div>
+      <div class="mt-1">
         {{ property.formattedPrice }}
         <span class="text-gray-600 text-sm"> / wk</span>
       </div>
       <div class="mt-2 flex items-center">
-        <span>{{ property.reviewCount }} reviews</span>
+        <span class="text-indigo-500 font-semibold"
+          >{{ property.rating }}/5 stars
+        </span>
+        <span class="ml-2 text-gray-600 text-sm"
+          >{{ property.reviewCount }} reviews</span
+        >
       </div>
     </div>
   </div>
