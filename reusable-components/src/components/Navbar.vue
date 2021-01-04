3fe87bd76e8d24485ba851a15972bebcbc557092
Joy Poddar <joypoddar.sd@gmail.com>
2021-01-04 22:21:20 +0530

    Building a Navbar Layout with Flexbox

---
+++ reusable-components/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex bg-gray-900 items-center justify-between px--4 py-3">
+    <div>
+      <img class="h-8" src="/img/logo-inverted.svg" alt="Workation" />
+    </div>
+    <div>
+      <button
+        type="button"
+        class="text-gray-500 focus:text-white hover:text-white focus:outline-none"
+      >
+        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+          <path
+            fill-rule="evenodd"
+            d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+          />
+        </svg>
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
